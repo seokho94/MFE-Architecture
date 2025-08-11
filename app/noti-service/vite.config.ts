ce0ea3d5b9be70bfcf90db35c4882edcd2bf817f
shhong <happt7889@gmail.com>
2025-08-11 16:45:49 +0900

    [feat][mfe][noti] Noti Service Module Federation 세팅

--- app/noti-service/vite.config.ts
+++ app/noti-service/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'noti_service',
+      filename: 'notiService.js',
+      exposes: {
+        './NotiServiceComponent': './src/App.tsx',
+        // './routes': './src/routes/routes.tsx',
+      },
+      shared: {
+        react: {
+          requiredVersion: '^19.1.0', // package.json에 맞게 수정
+        },
+        'react-dom': {
+          requiredVersion: '^19.1.0',
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  server: {
+    port: 8083,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    target: 'esnext', // 최신 브라우저 대상
+    // assetsDir: '', // 필요 시 설정
+    // rollupOptions: {}, // 고급 빌드 설정 시 사용
+  },
+  // base: '/', // publicPath 관련, 필요 시 설정
+});
